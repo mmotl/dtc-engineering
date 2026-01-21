314b67f9cef2f62c2fa57fd7117a9c3b5cc817f0
Matthias Motl <68462971+mmotl@users.noreply.github.com>
2026-01-21 13:12:22 +0000

    first pipeline

--- README.md
+++ README.md
@@ -15,3 +15,7 @@ docker rm `docker ps -aq`
 ```zsh
 docker run --entrypoint=bash -it python:3.13.11-slim
 ```
+
+```zsh
+docker run -it --entrypoint=bash -v $(pwd)/test:/app/test python:3.13.11-slim
+```
---
+++ pipeline/pipeline.py
@@ -0,0 +1,12 @@
+import sys
+import pandas as pd
+
+print('argument: ', sys.argv)
+month = int(sys.argv[1])
+
+df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
+print(df.head())
+
+df.to_parquet(f'output_{month}.parquet')
+
+print(f'hello pipeline, month={month}')
